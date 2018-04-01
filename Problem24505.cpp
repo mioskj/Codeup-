3b8d506aeebf6730aec539f3bc3483b74a39dc8b
mioskj <155489327@qq.com>
2018-04-01 16:04:01 +0800

    Create Problem24505.cpp

---
+++ Problem24505.cpp
@@ -0,0 +1,17 @@
+/*
+题目描述
+编写一个C程序，要求在屏幕上输出一下一行信息。
+
+This is my first c program!
+
+输入
+无
+输出
+This is my first c program!
+*/
+#include<cstdio>
+int main(){
+	char str[] = "This is my first c program!";
+	printf("%s",str);
+	return 0;
+}
