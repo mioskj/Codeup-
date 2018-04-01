17d6b8622c76f255fa2d61ee63efdd557399d8a3
mioskj <155489327@qq.com>
2018-04-01 16:15:49 +0800

    例题3-9 字符输入输出

---
+++ Problem25269.cpp
@@ -0,0 +1,17 @@
+/*
+题目描述
+从键盘输入三个字符BOY，然后把他们输出到屏幕上
+
+输入
+BOY三个字符，中间无分隔符
+
+输出
+BOY，注意末尾的换行
+*/
+#include<iostream>
+int main(){
+	char str[10];
+	scanf("%s",str);
+	printf("%s",str);
+	return 0;
+}
